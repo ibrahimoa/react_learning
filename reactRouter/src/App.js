26f316078c54677997facacc9850ae71b48556a8
ibrahimoa <ibraoa98@gmail.com>
2022-12-19 15:31:14 +0100

    chore: react router basics (section 20 1st part).

---
+++ reactRouter/src/App.js
@@ -0,0 +1,31 @@
+import { Fragment } from "react";
+import { Route, Switch, Redirect } from "react-router-dom";
+
+import Welcome from "./pages/Welcome";
+import Products from "./pages/Products";
+import MainHeader from "./components/MainHeader";
+import ProductDetail from "./pages/ProductDetail";
+
+function App() {
+  return (
+    <Fragment>
+      <MainHeader />
+      <Switch>
+        <Route path="/" exact>
+          <Redirect to="/welcome" />
+        </Route>
+        <Route path="/welcome">
+          <Welcome />
+        </Route>
+        <Route path="/products" exact>
+          <Products />
+        </Route>
+        <Route path="/products/:productId">
+          <ProductDetail />
+        </Route>
+      </Switch>
+    </Fragment>
+  );
+}
+
+export default App;
